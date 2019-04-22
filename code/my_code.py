d617f4e42de836564bbe6dbe50c16af44352deb2
Yu-Ju Chen <yuju.chen@agoop.co.jp>
2019-04-22 11:11:47 +0900

    add test code

---
+++ code/my_code.py
@@ -0,0 +1,12 @@
+def add(first, second):
+    """Add two number
+    
+    Args:
+        first (float): the first number
+        second (float): the second number
+    
+    Returns:
+        float: the summation of the two input numbers
+    """
+    result = first + second
+    return result
