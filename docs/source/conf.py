329f6256f9faa3dd70158cef9f88704470e32903
Yu-Ju Chen <yuju.chen@agoop.co.jp>
2019-04-22 12:14:15 +0900

    Update Sphinx result

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0,os.path.abspath('/Users/casbie/Project/probable-octo-giggle/code'))
 
 
 # -- Project information -----------------------------------------------------
@@ -48,6 +48,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
---
+++ docs/source/my_code.rst
@@ -0,0 +1,7 @@
+my\_code module
+===============
+
+.. automodule:: my_code
+    :members:
+    :undoc-members:
+    :show-inheritance:
